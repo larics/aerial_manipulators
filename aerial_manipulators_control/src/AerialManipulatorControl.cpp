211c898516a24d43571209b4760f245f4fe86e50
suiauthon <carek.marko@gmail.com>
2020-09-01 17:13:49 +0200

    solver testing

--- aerial_manipulators_control/src/AerialManipulatorControl.cpp
+++ aerial_manipulators_control/src/AerialManipulatorControl.cpp
@@ -739,6 +739,15 @@ class AerialManipulatorControl {
         };
 };
 
+struct CostFunctor {
+  template <typename T>
+  bool operator()(const T* const x, T* residual) const {
+    //if(x[0] >= static_cast<T>(9)) return false;
+    residual[0] = 10.0 - x[0];
+    return true;
+  }
+};
+
 
 int main(int argc, char **argv)
 {
@@ -807,6 +816,27 @@ int main(int argc, char **argv)
         ros::Duration(0.5).sleep();
     }
     ROS_INFO("[AerialManipulatorControl] Waiting aerial manipulator control to start...");
+
+    double x = 0.5;
+  const double initial_x = x;
+        // Build the problem.
+      ceres::Problem problem;
+      // Set up the only cost function (also known as residual). This uses
+      // auto-differentiation to obtain the derivative (jacobian).
+      ceres::CostFunction* cost_function =
+          new ceres::AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
+      problem.AddResidualBlock(cost_function, nullptr, &x);
+      // Run the solver!
+      //problem.AddParameterBlock(&x, 1);
+      problem.SetParameterUpperBound(&x, 0, 8);
+
+      ceres::Solver::Options options;
+      options.minimizer_progress_to_stdout = true;
+      ceres::Solver::Summary summary;
+      ceres::Solve(options, &problem, &summary);
+      std::cout << summary.BriefReport() << "\n";
+      std::cout << "x : " << initial_x
+                << " -> " << x << "\n";
 
     time_old = aerial_manipulator_control.getTime().toSec();
 
