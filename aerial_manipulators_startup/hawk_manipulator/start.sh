128522320fbc2ba49daf80a3ca1d63c46100280e
aivano <antun.ivanovic@fer.hr>
2022-05-03 14:34:48 +0200

    [Startup] Add option to start different session yml file.

--- aerial_manipulators_startup/hawk_manipulator/start.sh
+++ aerial_manipulators_startup/hawk_manipulator/start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Session input argument. Defaults to session.yml if not provided
+SESSION=${2:-session.yml}
+arr_split=(${SESSION//./ })
+extension=${arr_split[${#arr_split[@]}-1]}
+
+if [ "$extension" == "yml" ] || [ "$extension" == "yaml" ]; then
+  :
+else
+  SESSION="session.yml"
+fi
+
 # Absolute path to this script. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
@@ -10,7 +21,7 @@ cd "$SCRIPTPATH"
 rm .tmuxinator.yml
 
 # link the session file to .tmuxinator.yml
-ln session.yml .tmuxinator.yml
+ln $SESSION .tmuxinator.yml
 
 SETUP_NAME=$1
 [ -z "$SETUP_NAME" ] && SETUP_NAME=optitrack_setup.sh
