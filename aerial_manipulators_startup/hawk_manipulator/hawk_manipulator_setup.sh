41a71d92d54cdd2c9adef9e3c5e9a0905c4dabb0
Filip Zoric <fzoric8@gmail.com>
2023-04-06 12:53:27 +0200

    Added param fixes

--- aerial_manipulators_startup/hawk_manipulator/hawk_manipulator_setup.sh
+++ aerial_manipulators_startup/hawk_manipulator/hawk_manipulator_setup.sh
@@ -1,5 +1,5 @@
 source optitrack_setup.sh
 
-export CONTROL_PARAMS=custom_config/optitrack_hawk_manipulator_posctl_thrust.params.yaml
+export CONTROL_PARAMS=./custom_config/optitrack_hawk_manipulator_posctl_thrust.params.yaml
 export OBJECT_NAME=hawk1
 export ODOM_TOPIC=/$OBJECT_NAME/vrpn_client/estimated_odometry
